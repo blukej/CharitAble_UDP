1bb9a7b5f5e82476ab9446d3b839ae302f281004
D00156796 <D00156796@student.dkit.ie>
2019-10-30 19:01:51 +0000

    Added in bootstrap four and played around with some nav ideas

--- templates/layouts/main.php
+++ templates/layouts/main.php
@@ -5,12 +5,15 @@
 
   <link href="<?=BASE_DIR?>/assets/css/reset.css" rel='stylesheet'>
   <link href="<?=BASE_DIR?>/assets/css/site.css" rel='stylesheet'>
-  
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
   <meta name='viewport' content='width=device-width, initial-scale=1.0'>
   <title><?=$locals['title']?></title>
 </head>
 <body>
-<?=BASE_DIR?>
-  <?= \Rapid\Renderer::VIEW_PLACEHOLDER ?>
+
+    <?= \Rapid\Renderer::VIEW_PLACEHOLDER ?>
+  
+  
 </body>
 </html>
