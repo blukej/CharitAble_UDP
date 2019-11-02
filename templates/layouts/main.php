ae50f838f186dc2dd92cbc7b287c8902b8601eef
D00156796 <D00156796@student.dkit.ie>
2019-11-02 19:33:03 +0000

    conected css files to layouts

--- templates/layouts/main.php
+++ templates/layouts/main.php
@@ -3,8 +3,8 @@
 <head>
   <meta charset='UTF-8'>
 
-  <link href="<?=BASE_DIR?>/assets/css/reset.css" rel='stylesheet'>
-  <link href="<?=BASE_DIR?>/assets/css/site.css" rel='stylesheet'>
+  <link href="./assets/css/reset.css" rel='stylesheet'>
+  <link href="./assets/css/site.css" rel='stylesheet'>
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
   <meta name='viewport' content='width=device-width, initial-scale=1.0'>
@@ -12,6 +12,7 @@
 </head>
 <body>
 
+    <?=BASE_DIR?>
     <?= \Rapid\Renderer::VIEW_PLACEHOLDER ?>
   
   
