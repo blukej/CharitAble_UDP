11dfb24068012e2ca36905ca3edacb6cfe135a93
D00206766@dkit.student.ie <D00206766@student.dkit.ie>
2019-11-04 17:52:48 +0000

    Added missing registration files from previous commits
    In previous commits stating the addition of a registration system, I
    accidently did not add the registration files to the commits. In this
    commit I fix this error.

---
+++ controllers/RegisterProcess.php
@@ -0,0 +1,17 @@
+<?php return function($req, $res) { require_once('./models/Login.php'); 
+
+session_start();
+
+$db = \Rapid\Database::getPDO();
+
+$user = new Login([
+    'user_name' => $req->body('username'),
+    'email' => $req->body('email'),
+    'hash' => $req->body('password')
+]);
+
+$user->register($db);
+
+$res->redirect("/Login");
+
+}?>
