40eb2d812b28bf5ea403145fa54a2394ed0de8d8
D00156796 <D00156796@student.dkit.ie>
2019-11-05 12:43:20 +0000

    Reuploading menu

--- controllers/Home.php
+++ controllers/Home.php
@@ -1,6 +1,8 @@
 <?php return function($req, $res) {
 
-  $res->status(501);
-  $res->send('<p>The Home controller has not yet been implement. This is just a test.</p>');
+$res->render('main', 'example', [
+  'someLocalKey' => 'Hello There',
+  'title'   => 'Home'
+]);
 
 } ?>
