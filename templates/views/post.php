2f07da0347ca1ec092d18bf7bef55c9e563346b1
D00206766@dkit.student.ie <D00206766@student.dkit.ie>
2019-11-08 18:21:55 +0000

    Changed how posts are displayed

--- templates/views/post.php
+++ templates/views/post.php
@@ -12,8 +12,8 @@
 <?php foreach($locals['displayPosts'] as $display) : ?>
 <?php $count++; ?>
 
-<div class="panel-heading h2">Username:<?= $display["user_name"]; ?></div>
-<div class="panel-footer h2">Title:<?= $display["subject"]; ?></div>
-<div class="panel-footer h2">Content:<?= $display["text"]; ?></div>
+<p>Username:<?= $display["user_name"]; ?></p>
+<p>Title:<?= $display["subject"]; ?></p>
+<p>Content:<?= $display["text"]; ?></p>
 
 <?php endforeach; ?>
