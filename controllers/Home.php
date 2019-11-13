ca309464194cf73c19930d813a3d949494dfa122
D00156796 <D00156796@student.dkit.ie>
2019-11-13 12:47:46 +0000

    The new layout was created for the Loggedin user

--- controllers/Home.php
+++ controllers/Home.php
@@ -1,7 +1,7 @@
 <?php return function($req, $res) {
   session_start();
 
-$res->render('main', 'home', [
+$res->render('main', 'example', [
   'someLocalKey' => 'Hello There',
   'title'   => 'Home'
 ]);
