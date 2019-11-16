ca841bef16a277441c4fd50e8d185976c8c3619c
D00208417 <d00208417@student.dkit.ie>
2019-11-16 20:18:26 +0000

    Added donation button to posts
    Added paypal donations button to posts, will need to change it so it
    retrieves to email of the post it is attached to.

--- templates/views/post.php
+++ templates/views/post.php
@@ -55,6 +55,13 @@
   			<li><span><i class="glyphicon glyphicon-calendar"></i> <?= $displayComments["timestamp"]; ?> </span></li>
             <li>|</li>
             <span><i class="glyphicon glyphicon-comment"></i> 2 comments</span>
+            <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top" class="text-center">
+                <input type="hidden" name="cmd" value="_donations" />
+        <input type="hidden" name="business" value="lukec0399@gmail.com" />
+        <input type="hidden" name="currency_code" value="EUR" />
+        <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
+        <img alt="" border="0" src="https://www.paypal.com/en_IE/i/scr/pixel.gif" width="1" height="1" />
+        </form>
 			</ul>
        </div>
     </div>
