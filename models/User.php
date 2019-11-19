4061bb98a25132a63aab9cd913b669ccd8f82209
D00156796 <D00156796@student.dkit.ie>
2019-11-19 23:43:36 +0000

    The user card is started, but cannot get following as DB needs updaing.

--- models/User.php
+++ models/User.php
@@ -455,30 +455,41 @@ public static function findAllCharities($pdo) {
 
 public function displayCharityCard($following)
 {
-    
-    if(in_array($following, $this->getUserName()))
-    {
-        echo"Unfollow";
-    }
-    else{
-        echo"Follow";
-    }
-
+    $isFollowing = FALSE;
     ?>
-    <div class="col-md-4 mt-4">
-    		    <div class="card profile-card-5">
-    		        <div class="card-img-block">
-    		            <img class="card-img-top" src="./<?=$this->getURL()?>" alt="Card image cap">
-    		        </div>
-                    <div class="card-body pt-0">
-                    <h5 class="card-title"><?=$this->getUserName()?></h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="<?=$this->getUserName()?>" class="btn btn-primary">Profile</a>
-                  </div>
+    <div class="row">
+        <div class="card col-8">
+            <div class="card-header">
+                <div class="card-title">
+                <?=$this->getUserName()?>
                 </div>
-    		</div>
-    		
-    	</div>
+            </div>
+            <div class="card-body">
+                <div class="card-text">
+                <?=$this->getEmail()?>
+                </div>
+                <?php
+                
+                foreach($following as $followed)
+                {
+                    if($this->getUserName()==$followed['follow_user_name'])
+                    {
+                        $isFollowing = TRUE;
+                    }
+                }
+                if($isFollowing)
+                {
+                    ?>
+                    <div class="card-text">Unfollow</div>
+                    <?php
+                }else{
+                    ?>
+                    <div class="card-text">Follow</div>
+                    <?php
+                }
+                ?>
+            </div>
+        </div>
     </div>
     <?php
 }
